aaf2ae58be089add76cd55d121cfc6a6578034b7
Ferdinand <ferdifenrir@gmail.com>
2018-04-29 18:52:19 +0700

    [COLDFIX] redesign landing and about

--- src/components/about/AboutCard.js
+++ src/components/about/AboutCard.js
@@ -12,11 +12,12 @@ export default class AboutCard extends Component {
         <Card.Meta>
           <span className="color-primary">Frontend Engineer</span>
         </Card.Meta>
-        <Card.Description>Hubungi saya lewat dibawah ini</Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <Icon name="mail" />
-        ferdifenrir@gmail.com
+        <Card.Description>Informasi kontak</Card.Description>
+        <Icon name="mail" /> ferdifenrir@gmail.com <br />
+        <Icon name="github" /> ferzos <br />
+        <Icon name="linkedin" /> ferdinand-chandra-8a7aa011b <br />
       </Card.Content>
     </Card>
   );
